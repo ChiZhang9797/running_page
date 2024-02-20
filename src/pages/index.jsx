1a8a850e5a0d9d21925ad4fbdfd1e38f999768a1
Chi Zhang <aaronzhang9797@outlook.com>
2024-02-20 16:41:34 -0500

    Updated page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -169,6 +169,7 @@ const Index = () => {
       <div className="mb5">
         <div className="w-100">
           <h1 className="f1 fw9 i">
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/album/50o7kf2wLwVmOTVYJOTplm?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <a href="/">{siteTitle}</a>
           </h1>
         </div>
